917414d9c5b50a615f3595224611b057d4e4e927
kentur <kevinxrturop@gmail.com>
2024-09-20 14:30:21 -0500

    Update index.tsx

--- components/welcomeDoesWork/index.tsx
+++ components/welcomeDoesWork/index.tsx
@@ -4,29 +4,30 @@ import ServiceItem from "./serviceItem";
 import Link from "next/link";
 export default function HowDoesItWork() {
   const cardList = [
-    {
-      title: "You place products",
-      link: "/",
-      desc: "What should be placed in the institution?",
-      img: "/images/service1.png",
-    },
-    {
-      title: "QR Scanning",
-      link: "/",
-      desc: "Or attaches the phone to an NFC chip",
-      img: "/images/service2.png",
-    },
-    {
-      title: "Opening the menu",
-      link: "/",
-      desc: "Does not require installation",
-      img: "/images/service3.png",
-    },
+  {
+  title: "Colocas los productos",
+  link: "/",
+  desc: "¿Qué se debe colocar en el establecimiento?",
+  img: "/images/service1.png",
+},
+{
+  title: "Escaneo de QR",
+  link: "/",
+  desc: "O acerca el teléfono a un chip NFC",
+  img: "/images/service2.png",
+},
+{
+  title: "Abrir el menú",
+  link: "/",
+  desc: "No requiere instalación",
+  img: "/images/service3.png",
+},
+
   ];
   return (
     <div className="welcome-container">
       <div className={cls.container}>
-        <div className={cls.title}>How does it work?</div>
+        <div className={cls.title}>¿Cómo funciona?</div>
         <div className={cls.serviceItems}>
           {cardList.map((item, key) => (
             <ServiceItem key={key} item={item} />
@@ -35,15 +36,15 @@ export default function HowDoesItWork() {
         <div className={cls.bottomContent}>
           <div className={cls.left}>
             <div className={cls.text}>
-              Easily guided, the customer selects suitable dishes and makes an
-              order.
+             Guiado fácilmente, el cliente selecciona los platos adecuados y elabora una
+              orden.
             </div>
             <Link href="/about">
-              <div className={cls.seeAllBtn}>See All</div>
+              <div className={cls.seeAllBtn}>Ver todo</div>
             </Link>
           </div>
           <div className={cls.right}>
-            Everything your customers need in one service
+           Todo lo que tus clientes necesitan en un solo servicio
           </div>
         </div>
       </div>
